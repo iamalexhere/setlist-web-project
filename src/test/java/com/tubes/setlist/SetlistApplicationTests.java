93aa7da3fed4dd2eab115dbc0cf48b4fd2dcf4f4
Imanuel Alexander Here <i.alex.here2004@gmail.com>
2024-11-28 17:34:41 +0700

    init spring project

---
+++ src/test/java/com/tubes/setlist/SetlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tubes.setlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
